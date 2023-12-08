0b3abeabf65bfc5d2ccb52848b325ea91629efed
ASHISH CHAUHAN <ASHISH CHAUHAN@LAPTOP-DI82GNMJ>
2023-12-08 12:52:55 +0530

    First commit for DBAS

---
+++ src/main/java/com/cc/dbas/CcDbasApplication.java
@@ -0,0 +1,13 @@
+package com.cc.dbas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcDbasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcDbasApplication.class, args);
+	}
+
+}
